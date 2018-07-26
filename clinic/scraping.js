8f1b48b6837419125f14ebb5770e1dbbc341911b
hikarut <ht.hikaru.takahashi@gmail.com>
2018-07-26 23:08:22 +0900

    htmlを出力に追加

--- clinic/scraping.js
+++ clinic/scraping.js
@@ -25,9 +25,8 @@ console.log(clinicPageUrl);
 
 if (pageNumber === 1) {
   // ファイルのヘッダー
-  // let header = '院名\t住所\t最寄り駅\t診療時間\n';
-  // let header = '院名\t住所\t最寄り駅1\t最寄り駅2\t最寄り駅3\t診療時間\n';
   let header = '院名\t郵便番号\t住所\t最寄り駅1\t最寄り駅2\t最寄り駅3\t診療時間\n';
+  // let header = '院名\t郵便番号\t住所\t最寄り駅1\t最寄り駅2\t最寄り駅3\t診療時間\t診療時間html\n';
   // tsvに書き込み
   fs.appendFile(tsvFile, header, (err) => {
     if (err) {
@@ -49,12 +48,16 @@ let fileData = '';
   const data = await page.evaluate(() => {
     // 以下ブラウザで動くコードを書く
 
+    // 欲しいデータの初期化
+    // var dataList = [];
+    var dataList = {};
+    dataList.text = [];
+    dataList.url = [];
+
     // メインのコンテンツだけに絞る
     var data = document.getElementsByClassName("maintop4");
     var item = data[0].getElementsByClassName("item");
 
-    // 欲しいデータの初期化
-    var dataList = [];
     for (var i = 0; i < item.length; ++i){
       // 院名
       var nameBox = item[i].getElementsByClassName("name_box");
@@ -64,6 +67,15 @@ let fileData = '';
       } else {
         nameBox = '';
       }
+
+      // 病院の詳細ページのURLを取得
+      var nameUrl = item[i].getElementsByTagName('a');
+      if (nameUrl[0] !== undefined) {
+        nameUrl = nameUrl[0].getAttribute('href');
+      } else {
+        nameUrl = '';
+      }
+      dataList.url.push(nameUrl);
 
       // 住所
       var access = item[i].getElementsByClassName("access");
@@ -108,23 +120,22 @@ let fileData = '';
       if (time[0] !== undefined) {
         // 改行を全て削除
         time = time[0].innerText.replace(/\r?\n/g,'');
+        // タブを全てスペースに置換
+        time = time.replace(/\t/g,' ');
       } else {
         time = '';
       }
 
-      var output = `${nameBox}\t${access}\t${typeTrain1}\t${typeTrain2}\t${typeTrain3}\t${time}`
-      dataList.push(output);
+      var output = `${nameBox}\t${access}\t${typeTrain1}\t${typeTrain2}\t${typeTrain3}\t${time}\t${nameUrl}`
+      dataList.text.push(output);
     }
     return dataList;
   });
 
-  await browser.close();
-
   // 郵便番号のデータを取得
   const postalCodeData = await readPostalCodeFile();
-
   // ファイルへの書き込みするデータの処理
-  for (let value of data) {
+  for (let value of data.text) {
     // 全郵便番号と総当たりで検索する
     let postalCode = '';
     for (let postal of postalCodeData) {
@@ -135,14 +146,32 @@ let fileData = '';
       }
     }
 
-    // fileData += `${value}\n`;
-
     // 一旦配列に分割する
     const valueArray = value.split('\t');
+
+    console.log('詳細ページに遷移');
+    const url = valueArray[6];
+    console.log(url);
+    await page.goto(url);
+    const htmlData = await page.evaluate(() => {
+      // 以下ブラウザで動くコードを書く
+      var table = document.getElementsByClassName("time_table");
+      var html = '';
+      // htmlをそのまま返す
+      if (table[0] !== undefined) {
+        html = table[0].outerHTML;
+        // 改行を全て削除
+        html = html.replace(/\r?\n/g,'');
+      }
+      return html
+    });
+
     // 順番を変えて郵便番号を追加する
-    fileData += `${valueArray[0]}\t${postalCode}\t${valueArray[1]}\t${valueArray[2]}\t${valueArray[3]}\t${valueArray[4]}\t${valueArray[5]}\n`;
-    // fileData += `${lineData}\n`;
+    fileData += `${valueArray[0]}\t${postalCode}\t${valueArray[1]}\t${valueArray[2]}\t${valueArray[3]}\t${valueArray[4]}\t${htmlData}\n`;
+    // fileData += `${valueArray[0]}\t${postalCode}\t${valueArray[1]}\t${valueArray[2]}\t${valueArray[3]}\t${valueArray[4]}\t${valueArray[5]}\t${url}\n`;
   }
+
+  await browser.close();
 
   // tsvに書き込み
   fs.appendFile(tsvFile, fileData, (err) => {
